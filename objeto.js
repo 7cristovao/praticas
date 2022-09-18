41b4165c6dc739974f1a1c84a55c501c3e8a33d7
7cristovao <cristovao.rocha@gmail.com>
2022-09-18 14:07:46 -0300

    exemplo de objetos

---
+++ objeto.js
@@ -0,0 +1,12 @@
+let pessoa = {
+    //chave: "valor",
+    nome: "Alex",
+    time: "Corinthians",
+    altura: 1.5, 
+    trabalhando: true
+    // obs: não usar números como chave de um objeto
+}
+
+console.log(pessoa['nome']) // resgata o atributo nome (posição)
+console.log(pessoa.time) // resgata pela chave (posição)
+console.log(pessoa)
---
+++ time.js
@@ -0,0 +1,63 @@
+// Exemplo: seleção de futebol campeã da Copa do Mundo de 2002
+
+let time = [
+    {
+        nome:"Dida",
+        camisa: 1,
+        posicao: "goleiro"
+    },
+    {
+        nome:"Roque Jr.",
+        camisa: 3,
+        posicao: "zagueiro"
+    },
+    {
+        nome: "Edmilson",
+        camisa: 5,
+        posicao: "zagueiro"
+    },
+    {
+        nome:"Júnior Baiano",
+        camisa: 4,
+        posicao: "zagueiro"
+    },
+    {
+        nome:"Cafú",
+        camisa:2,
+        posicao: "lateral"
+    },
+    {
+        nome:"Roberto Carlos",
+        camisa: 6,
+        posicao: "lateral"
+    },
+    {
+        nome:"Gilberto Silva",
+        camisa:8,
+        posicao: "volante"
+    },
+    {
+        nome:"Kleberson",
+        camisa:18,
+        posicao: "meio-campo"
+    },
+    {
+        nome:"Rivaldo",
+        camisa:10,
+        posicao: "meio-campo"
+    },
+    {
+        nome:"Ronaldinho",
+        camisa:11,
+        posicao: "atacante"
+    },
+    {
+        nome:"Ronaldo",
+        camisa:9,
+        posicao: "atacante"
+
+    }
+]
+
+console.log(time.length)
+console.table(time) // funcao que monta uma tabela
