dad3bfaf832ed04787a8af02588d417f492f357f
7cristovao <cristovao.rocha@gmail.com>
2022-09-18 15:33:13 -0300

    exemplo de objetos e novas atribuiçoes

---
+++ teste.js
@@ -0,0 +1,18 @@
+/*
+// ordem crescente
+for (
+    let i = 0; // inicializa do zero
+    i<5; // condicao
+    i++ // incrementa
+    ){
+    console.log(i)
+}
+*/
+//ordem decrescente
+for (
+    let i = 4; // inicializa do quatro
+    i >= 0; // condicao
+    i-- //decrementa
+    ){
+        console.log(i)
+    }
--- time.js
+++ time.js
@@ -59,5 +59,52 @@ let time = [
     }
 ]
 
+/*
 console.log(time.length)
 console.table(time) // funcao que monta uma tabela
+*/
+
+/*
+for (let i = 0; i<time.length;i++){
+    console.log(i, time[i])
+}
+*/
+
+// forin (vai de um em um e perfila até o final)
+// no key guarda a posição do objeto/vetor
+/*
+    for (const key in time) {
+    console.log(key,time[key])
+}
+*/
+
+// forof (inves de guardar a posicao ele guarda o valor)
+/*
+for (const jogador of time) {
+    console.log(jogador)
+}
+
+console.table(time)
+*/
+/*
+for (const i in time) {
+    //time[i].multiplo = time[i].camisa * 2
+    // multiplo é um atributo novo
+    // camisa é um atributo existente
+    // colocar objeto_vetor[posicao]
+    /*
+    // também serve
+    
+    time[i]["multiplo"] = time[i]["camisa"] * 2
+}
+
+console.table(time)
+*/
+
+// key ou i ou posição são sinonimos
+
+for (const jogador of time) {
+    jogador["multiplo"] = jogador["camisa"]*2
+    //jogador.multiplo = jogador.camisa*2
+}
+console.table(time)
